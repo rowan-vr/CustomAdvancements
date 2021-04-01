eecdf078b4fbed2e93ae302e99cba56fd3f8b391
Red-Dev-Red <rowanmtvanrooijen@gmail.com>
2021-04-01 15:37:06 +0200

    Added file saving on quit

---
+++ src/main/java/me/tippie/customadvancements/listeners/PlayerJoinQuitListener.java
@@ -0,0 +1,24 @@
+package me.tippie.customadvancements.listeners;
+
+import me.tippie.customadvancements.CustomAdvancements;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.logging.Level;
+
+public class PlayerJoinQuitListener implements Listener {
+
+	@EventHandler
+	private void onJoin(final PlayerJoinEvent event) {
+		CustomAdvancements.getInstance().getLogger().log(Level.INFO, "Attempting to load player: " + event.getPlayer().getName());
+		CustomAdvancements.getCaPlayerManager().loadPlayer(event.getPlayer());
+	}
+
+	@EventHandler
+	private void onDisconnect(final PlayerQuitEvent event) {
+		CustomAdvancements.getCaPlayerManager().savePlayer(event.getPlayer());
+		CustomAdvancements.getCaPlayerManager().unloadPlayer(event.getPlayer());
+	}
+}
