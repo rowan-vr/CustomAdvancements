15d989ffd0abea340c61a543b795ffe5d44243c5
Red-Dev-Red <rowanmtvanrooijen@gmail.com>
2021-04-07 16:45:09 +0200

    Added playtime for a specific world

--- src/main/java/me/tippie/customadvancements/advancement/types/Playtime.java
+++ src/main/java/me/tippie/customadvancements/advancement/types/Playtime.java
@@ -6,20 +6,34 @@ import me.tippie.customadvancements.util.Lang;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Playtime extends AdvancementType {
 	public Playtime() {
 		super("playtime", Lang.ADVANCEMENT_TYPE_PLAYTIME_UNIT.getString());
-		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(CustomAdvancements.getInstance(), new Runnable() {
-			@Override public void run() {
-				for (final Player player : Bukkit.getServer().getOnlinePlayers()) {
-					progress(player, player.getUniqueId());
-				}
+		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(CustomAdvancements.getInstance(), () -> {
+			for (final Player player : Bukkit.getServer().getOnlinePlayers()) {
+				progress(player, player.getUniqueId());
 			}
 		}, 10L, 1200L);
 	}
 
-	@Override protected void onProgress(final Object e, final String value, final String path) {
+	@Override protected void onProgress(final Object e, String value, final String path) {
 		val player = (Player) e;
-		progression(1, path, player.getUniqueId());
+		if (value == null || value.equalsIgnoreCase("any") || value.equalsIgnoreCase("")) {
+			progression(1, path, player.getUniqueId());
+		} else {
+			val worldName = player.getWorld().getName();
+			boolean not = false;
+			if (value.startsWith("!")) {
+				value = value.toLowerCase().substring(1);
+				not = true;
+			}
+			List<String> worlds = new ArrayList<>(Arrays.asList(value.split(",")));
+			if ((worlds.contains(worldName) && !not) || (!worlds.contains(worldName) && not))
+				progression(1, path, player.getUniqueId());
+		}
 	}
 }
