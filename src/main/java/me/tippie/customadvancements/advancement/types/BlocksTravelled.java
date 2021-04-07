412daee24ff34e7c19760f8d819c92a1c5ed9d72
Red-Dev-Red <rowanmtvanrooijen@gmail.com>
2021-04-07 13:39:46 +0200

    Fixed BlocksTravelled, now actually counts blocks instead of something else

--- src/main/java/me/tippie/customadvancements/advancement/types/BlocksTravelled.java
+++ src/main/java/me/tippie/customadvancements/advancement/types/BlocksTravelled.java
@@ -14,7 +14,7 @@ public class BlocksTravelled extends AdvancementType {
 	@EventHandler
 	public void onBlockMove(final PlayerMoveEvent e) {
 		if (e.getTo() == null) return;
-		if (e.getFrom().getX() != e.getTo().getX() || e.getFrom().getZ() != e.getTo().getZ()) {
+		if ((int) e.getFrom().getX() != (int) e.getTo().getX() || (int) e.getFrom().getZ() != (int) e.getTo().getZ()) {
 			progress(e, e.getPlayer().getUniqueId());
 		}
 	}
@@ -22,6 +22,6 @@ public class BlocksTravelled extends AdvancementType {
 	@Override protected void onProgress(final Object e, final String value, final String path) {
 		val event = (PlayerMoveEvent) e;
 		if (event.getTo() == null) return;
-		progression((int) event.getFrom().distance(event.getTo()), path, event.getPlayer().getUniqueId());
+		progression((int) Math.max(1, Math.round(event.getFrom().distance(event.getTo()))), path, event.getPlayer().getUniqueId());
 	}
 }
