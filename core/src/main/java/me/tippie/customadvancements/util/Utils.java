4799db8752d6c3546f9885a6c999b37aebf1d822
Tippie <rowan@tippie.me>
2021-04-13 15:29:46 +0200

    Progression on Toasts

--- core/src/main/java/me/tippie/customadvancements/util/Utils.java
+++ core/src/main/java/me/tippie/customadvancements/util/Utils.java
@@ -8,12 +8,31 @@ import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
 
 public class Utils {
-    public static void showToast(Player player){
+
+    // Format for JSON component:
+    // https://minecraft.fandom.com/wiki/Advancement/JSON_format
+    public static void showToast(Player player, String title, String description){
         JsonObject advancementJSON = new JsonObject();
+
         JsonObject display = new JsonObject();
         display.addProperty("show_toast", true);
+        display.addProperty("announce_to_chat", false);
+        display.addProperty("hidden",true);
+        display.addProperty("frame", "task");
+        display.addProperty("title", title);
+        display.addProperty("description", description);
+
+        JsonObject icon = new JsonObject();
+        icon.addProperty("item", "minecraft:stone");
+
+        display.add("icon", icon);
+
+
         advancementJSON.add("display",display);
         NamespacedKey key = new NamespacedKey(CustomAdvancements.getInstance(), "showtoast"+Math.round(Math.random()*10000));
         Advancement advancement = Bukkit.getUnsafe().loadAdvancement(key, String.valueOf(advancementJSON));
+
+
+        Bukkit.getUnsafe().removeAdvancement(key);
     }
 }
