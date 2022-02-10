965db6e2f658fe379ba5f7701979a62dc6cb94a8
TippieCodes <rowan@tippie.me>
2022-02-10 01:31:00 +0100

    Bug fixes with BrewPotion advancement Type

--- src/main/java/me/tippie/customadvancements/advancement/types/BrewPotion.java
+++ src/main/java/me/tippie/customadvancements/advancement/types/BrewPotion.java
@@ -1,8 +1,12 @@
 package me.tippie.customadvancements.advancement.types;
 
 import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BrewingStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.BrewerInventory;
 import org.bukkit.inventory.ItemStack;
@@ -11,60 +15,78 @@ import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.lang.reflect.Field;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class BrewPotion extends AdvancementType{
+public class BrewPotion extends AdvancementType {
 
-    public BrewPotion() {
-        super("brew", "potions");
-    }
+	public BrewPotion() {
+		super("brew", "potions");
+	}
 
-    private final HashMap<ItemStack, UUID> itemTrackMap = new HashMap<>();
+	private final HashMap<Block, HashMap<Integer, UUID>> itemTrackMap = new HashMap<>();
 
-    @EventHandler
-    public void onInventoryPut(InventoryMoveItemEvent event){
-        if (event.getDestination() instanceof BrewerInventory && event.getSource() instanceof PlayerInventory) {
-            itemTrackMap.put(event.getItem(),((Player) event.getSource().getHolder()).getUniqueId());
-        } else if (!(event.getDestination() instanceof BrewerInventory) && event.getSource() instanceof BrewerInventory) {
-            itemTrackMap.remove(event.getItem());
-        }
-    }
+	@EventHandler
+	public void onBrewBreak(BlockBreakEvent event) {
+		if (event.getBlock().getType().equals(Material.BREWING_STAND))
+			itemTrackMap.keySet().removeIf(k -> k.getX() == event.getBlock().getX() && k.getY() == event.getBlock().getY() && k.getZ() == event.getBlock().getZ());
 
-    @EventHandler
-    public void onBrew(BrewEvent e){
-        for (int i = 0; i < 3; i++){
-            ItemStack potion = e.getContents().getItem(i);
-            UUID uuid = itemTrackMap.get(potion);
-            if (uuid != null)
-                progress(potion,uuid);
-        }
-    }
+	}
 
-    @Override
-    protected void onProgress(Object e, String value, String path) {
-        ItemStack potion = (ItemStack) e;
-        UUID uuid = itemTrackMap.remove(potion);
-        PotionMeta meta = (PotionMeta) potion.getItemMeta();
-        Set<PotionEffectType> effects = meta.getCustomEffects().stream().map(PotionEffect::getType).collect(Collectors.toSet());
-        if (value == null || value.equalsIgnoreCase("any")) {
-            progression(1,path,uuid);
-        } else {
-            boolean not = false;
-            if (value.startsWith("!")) {
-                value = value.substring(1);
-                not = true;
-            }
-            final List<PotionEffectType> types = new ArrayList<>();
-            final String[] typeStrings = value.split(",");
-            for (final String typeString : typeStrings)
-                types.add(PotionEffectType.getByName(typeString.toUpperCase()));
+	@EventHandler
+	public void onInventoryPut(InventoryClickEvent event) {
+		if (!(event.getClickedInventory() instanceof BrewerInventory) || event.getRawSlot() > 2) return;
+		itemTrackMap.computeIfAbsent(((BrewerInventory) event.getClickedInventory()).getHolder().getBlock(), k -> new HashMap<>());
+		HashMap<Integer, UUID> trackMap = itemTrackMap.get(((BrewerInventory) event.getClickedInventory()).getHolder().getBlock());
 
-            for (PotionEffectType type: effects) {
-                if ((types.contains(type) && !not) || (!types.contains(type) && not)) {
-                    progression(1, path, uuid);
-                }
-            }
-        }
-    }
+		if ((event.isLeftClick() || event.isRightClick()) && event.getCurrentItem() != null && !event.getCurrentItem().getType().equals(Material.AIR)) {
+			trackMap.put(event.getRawSlot(), null);
+		}
+		if ((event.isLeftClick() || event.isRightClick()) && event.getCursor() != null && !event.getCursor().getType().equals(Material.AIR))
+			trackMap.put(event.getRawSlot(), event.getWhoClicked().getUniqueId());
+
+	}
+
+	@EventHandler
+	public void onBrew(BrewEvent e) throws NoSuchFieldException, IllegalAccessException {
+		Field field = e.getClass().getDeclaredField("results");
+		field.setAccessible(true);
+		ArrayList<ItemStack> results = (ArrayList<ItemStack>) field.get(e);
+		for (int i = 0; i < 3; i++) {
+			if (itemTrackMap.get(e.getContents().getHolder().getBlock()) == null) continue;
+			UUID uuid = itemTrackMap.get(e.getContents().getHolder().getBlock()).get(i);
+			if (uuid != null) {
+				progress(new AbstractMap.SimpleEntry<>(uuid, results.get(i)), uuid);
+			}
+		}
+	}
+
+	@Override
+	protected void onProgress(Object e, String value, String path) {
+		AbstractMap.SimpleEntry<UUID, ItemStack> event = (AbstractMap.SimpleEntry<UUID, ItemStack>) e;
+		ItemStack potion = event.getValue();
+		UUID uuid = event.getKey();
+		PotionMeta meta = (PotionMeta) potion.getItemMeta();
+		PotionEffectType effect = meta.getBasePotionData().getType().getEffectType();
+		if (value == null || value.equalsIgnoreCase("any")) {
+			progression(1, path, uuid);
+		} else {
+			boolean not = false;
+			if (value.startsWith("!")) {
+				value = value.substring(1);
+				not = true;
+			}
+			final List<PotionEffectType> types = new ArrayList<>();
+			final String[] typeStrings = value.split(",");
+			for (final String typeString : typeStrings)
+				types.add(PotionEffectType.getByName(typeString));
+
+
+			if ((types.contains(effect) && !not) || (!types.contains(effect) && not)) {
+				progression(1, path, uuid);
+			}
+
+		}
+	}
 }
