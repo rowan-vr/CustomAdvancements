bbcfbe19db78c03e6fef89e220efeaf80b3fe309
Rowan <rowan@tippie.me>
2023-04-15 15:42:01 +0200

    Added validation for keys to inform users about this.d is null error (#122)

--- core/src/main/java/me/tippie/customadvancements/advancement/AdvancementManager.java
+++ core/src/main/java/me/tippie/customadvancements/advancement/AdvancementManager.java
@@ -23,202 +23,210 @@ import java.util.logging.Level;
  */
 public class AdvancementManager {
 
-	/**
-	 * Map with key type label and value the {@link AdvancementType} belonging to it.
-	 */
-	private final Map<String, AdvancementType> advancementTypes = new HashMap<>();
-	/**
-	 * Map with key tree label and value the {@link AdvancementTree} belonging to it.
-	 */
-	private final Map<String, AdvancementTree> advancementTrees = new HashMap<>();
-	/**
-	 * Map with key tree label and value the {@link AdvancementRewardType} belonging to it.
-	 */
-	private final Map<String, AdvancementRewardType> advancementRewardTypes = new HashMap<>();
-	/**
-	 * Map with key tree label and value the {@link AdvancementRewardType} belonging to it.
-	 */
-	private final Map<String, AdvancementRequirementType> advancementRequirementTypes = new HashMap<>();
+    /**
+     * Map with key type label and value the {@link AdvancementType} belonging to it.
+     */
+    private final Map<String, AdvancementType> advancementTypes = new HashMap<>();
+    /**
+     * Map with key tree label and value the {@link AdvancementTree} belonging to it.
+     */
+    private final Map<String, AdvancementTree> advancementTrees = new HashMap<>();
+    /**
+     * Map with key tree label and value the {@link AdvancementRewardType} belonging to it.
+     */
+    private final Map<String, AdvancementRewardType> advancementRewardTypes = new HashMap<>();
+    /**
+     * Map with key tree label and value the {@link AdvancementRewardType} belonging to it.
+     */
+    private final Map<String, AdvancementRequirementType> advancementRequirementTypes = new HashMap<>();
 
 
-	/**
-	 * Makes new {@link AdvancementManager}
-	 */
-	public AdvancementManager() {
-	}
+    /**
+     * Makes new {@link AdvancementManager}
+     */
+    public AdvancementManager() {
+    }
 
-	/**
-	 * Registers an advancement type with the plugin, example usage:
-	 * {@code advancementManager.registerAdvancement(new ExampleType());}
-	 *
-	 * @param advancementType the instance of an advancement type
-	 */
-	public void registerAdvancement(final AdvancementType advancementType) {
-		CustomAdvancements.getInstance().getServer().getPluginManager().registerEvents(advancementType, CustomAdvancements.getInstance());
-		advancementTypes.put(advancementType.getLabel(), advancementType);
-	}
+    /**
+     * Registers an advancement type with the plugin, example usage:
+     * {@code advancementManager.registerAdvancement(new ExampleType());}
+     *
+     * @param advancementType the instance of an advancement type
+     */
+    public void registerAdvancement(final AdvancementType advancementType) {
+        CustomAdvancements.getInstance().getServer().getPluginManager().registerEvents(advancementType, CustomAdvancements.getInstance());
+        advancementTypes.put(advancementType.getLabel(), advancementType);
+    }
 
-	/**
-	 * Registers an advancement reward type with the plugin, example usage:
-	 * {@code advancementManager.registerAdvancementReward(new ExampleType());}
-	 *
-	 * @param advancementRewardType the instance of an advancement type
-	 */
-	public void registerAdvancementReward(final AdvancementRewardType advancementRewardType) {
-		advancementRewardTypes.put(advancementRewardType.getLabel(), advancementRewardType);
-	}
+    /**
+     * Registers an advancement reward type with the plugin, example usage:
+     * {@code advancementManager.registerAdvancementReward(new ExampleType());}
+     *
+     * @param advancementRewardType the instance of an advancement type
+     */
+    public void registerAdvancementReward(final AdvancementRewardType advancementRewardType) {
+        advancementRewardTypes.put(advancementRewardType.getLabel(), advancementRewardType);
+    }
 
 
-	/**
-	 * Registers an advancement requirement type with the plugin, example usage:
-	 * {@code advancementManager.registerAdvancementRequirement(new ExampleType());}
-	 *
-	 * @param advancementRequirementType the instance of an advancement type
-	 */
-	public void registerAdvancementRequirement(final AdvancementRequirementType advancementRequirementType) {
-		advancementRequirementTypes.put(advancementRequirementType.getLabel(), advancementRequirementType);
-	}
+    /**
+     * Registers an advancement requirement type with the plugin, example usage:
+     * {@code advancementManager.registerAdvancementRequirement(new ExampleType());}
+     *
+     * @param advancementRequirementType the instance of an advancement type
+     */
+    public void registerAdvancementRequirement(final AdvancementRequirementType advancementRequirementType) {
+        advancementRequirementTypes.put(advancementRequirementType.getLabel(), advancementRequirementType);
+    }
 
-	/**
-	 * Loads the advancement trees and puts them into {@link AdvancementManager#advancementTrees}
-	 */
-	public void loadAdvancements() {
-		advancementTrees.clear();
-		final Path advancementFolder = Paths.get(CustomAdvancements.getInstance().getDataFolder() + "/advancement-trees");
-		if (!Files.exists(advancementFolder)) {
-			try {
-				Files.createDirectories(advancementFolder);
-				CustomAdvancements.getInstance().saveResource("advancement-trees/example.yml", false);
-			} catch (final IOException e) {
-				CustomAdvancements.getInstance().getLogger().log(Level.SEVERE, "Failed to read and/or create plugin directory.",e);
-			}
-		}
-		final File dir = new File(advancementFolder.toString());
-		final File[] advancementDirectoryContent = dir.listFiles();
-		assert advancementDirectoryContent != null;
-		for (final File file : advancementDirectoryContent) {
-			if (file.getName().endsWith(".yml")) {
-				AdvancementTree tree = new AdvancementTree(file);
-				if (file.getName().split(".yml")[0].contains(" ")){
-					CustomAdvancements.getInstance().getLogger().log(Level.SEVERE, "Advancement tree file name '"+file.getName()+"' contains spaces, this is not allowed.");
-					continue;
-				}
-				advancementTrees.put(file.getName().split(".yml")[0], tree);
-			}
-		}
-	}
+    /**
+     * Loads the advancement trees and puts them into {@link AdvancementManager#advancementTrees}
+     */
+    public void loadAdvancements() {
+        advancementTrees.clear();
+        final Path advancementFolder = Paths.get(CustomAdvancements.getInstance().getDataFolder() + "/advancement-trees");
+        if (!Files.exists(advancementFolder)) {
+            try {
+                Files.createDirectories(advancementFolder);
+                CustomAdvancements.getInstance().saveResource("advancement-trees/example.yml", false);
+            } catch (final IOException e) {
+                CustomAdvancements.getInstance().getLogger().log(Level.SEVERE, "Failed to read and/or create plugin directory.", e);
+            }
+        }
+        final File dir = new File(advancementFolder.toString());
+        final File[] advancementDirectoryContent = dir.listFiles();
+        assert advancementDirectoryContent != null;
+        for (final File file : advancementDirectoryContent) {
+            if (file.getName().endsWith(".yml")) {
+                try {
+                    AdvancementTree tree = new AdvancementTree(file);
+                    if (file.getName().split(".yml")[0].contains(" ")) {
+                        CustomAdvancements.getInstance().getLogger().log(Level.SEVERE, "Advancement tree file name '" + file.getName() + "' contains spaces, this is not allowed.");
+                        continue;
+                    }
+                    advancementTrees.put(file.getName().split(".yml")[0], tree);
+                } catch (IllegalArgumentException e) {
+                    CustomAdvancements.getInstance().getLogger().log(Level.SEVERE, "Advancement tree has invalid name '" + file.getName() + "'. Make sure it only contains letters, numbers and underscores.");
+                }
+            }
+        }
+    }
 
-	/**
-	 * Executes the complete complete actions for an advancement and marks it as completed
-	 *
-	 * @param path       The path of an advancement formatted as 'treeLabel.advancementLabel'
-	 * @param playeruuid UUID of the minecraft player
-	 */
-	public void complete(final String path, final UUID playeruuid) throws InvalidAdvancementException {
-		val treeLabel = getAdvancementTreeLabel(path);
-		val advancementLabel = getAdvancementLabel(path);
-		getAdvancementTree(treeLabel).complete(advancementLabel,playeruuid);
-	}
+    /**
+     * Executes the complete actions for an advancement and marks it as completed
+     *
+     * @param path       The path of an advancement formatted as 'treeLabel.advancementLabel'
+     * @param playeruuid UUID of the minecraft player
+     */
+    public void complete(final String path, final UUID playeruuid) throws InvalidAdvancementException {
+        val treeLabel = getAdvancementTreeLabel(path);
+        val advancementLabel = getAdvancementLabel(path);
+        getAdvancementTree(treeLabel).complete(advancementLabel, playeruuid);
+    }
 
-	/**
-	 * Searches for the advancement type using the label it is registered with.
-	 * @param type the label of an advancement type
-	 * @return the {@link AdvancementType}
-	 */
-	public AdvancementType getAdvancementType(final String type) {
-		return advancementTypes.values().stream().filter(advancement -> advancement.equals(type)).findAny().orElseGet(Empty::new);
-	}
+    /**
+     * Searches for the advancement type using the label it is registered with.
+     *
+     * @param type the label of an advancement type
+     * @return the {@link AdvancementType}
+     */
+    public AdvancementType getAdvancementType(final String type) {
+        return advancementTypes.values().stream().filter(advancement -> advancement.equals(type)).findAny().orElseGet(Empty::new);
+    }
 
-	/**
-	 * Converts map {@link AdvancementManager#advancementTrees} into a list and returns it.
-	 * @return list of all registered {@link AdvancementTree}'s
-	 */
-	public List<AdvancementTree> getAdvancementTrees() {
-		return new ArrayList<>(advancementTrees.values());
-	}
+    /**
+     * Converts map {@link AdvancementManager#advancementTrees} into a list and returns it.
+     *
+     * @return list of all registered {@link AdvancementTree}'s
+     */
+    public List<AdvancementTree> getAdvancementTrees() {
+        return new ArrayList<>(advancementTrees.values());
+    }
 
-	/**
-	 * Searches {@link AdvancementManager#advancementTrees} for the tree matching the label
-	 *
-	 * @param label the unique label of an {@link AdvancementTree}
-	 * @return the {@link AdvancementTree} of the label given as input
-	 */
-	public AdvancementTree getAdvancementTree(final String label) throws InvalidAdvancementException {
-		if (advancementTrees.get(label) == null) throw new InvalidAdvancementException();
-		return advancementTrees.get(label);
-	}
+    /**
+     * Searches {@link AdvancementManager#advancementTrees} for the tree matching the label
+     *
+     * @param label the unique label of an {@link AdvancementTree}
+     * @return the {@link AdvancementTree} of the label given as input
+     */
+    public AdvancementTree getAdvancementTree(final String label) throws InvalidAdvancementException {
+        if (advancementTrees.get(label) == null) throw new InvalidAdvancementException();
+        return advancementTrees.get(label);
+    }
 
-	/**
-	 * Converts map {@link AdvancementManager#advancementTypes} into a list and returns it.
-	 * @return list of all registered {@link AdvancementType}'s
-	 */
-	public List<AdvancementType> getAdvancementTypes() {
-		return new ArrayList<>(advancementTypes.values());
-	}
+    /**
+     * Converts map {@link AdvancementManager#advancementTypes} into a list and returns it.
+     *
+     * @return list of all registered {@link AdvancementType}'s
+     */
+    public List<AdvancementType> getAdvancementTypes() {
+        return new ArrayList<>(advancementTypes.values());
+    }
 
-	/**
-	 * Gets the correct advancement from the correct tree registered.
-	 *
-	 * @param path The path of an advancement formatted as 'treeLabel.advancementLabel'
-	 * @return the {@link CAdvancement} belonging to that path
-	 */
-	public CAdvancement getAdvancement(final String path) throws InvalidAdvancementException {
-		val treeLabel = getAdvancementTreeLabel(path);
-		val advancementLabel = getAdvancementLabel(path);
-		if (treeLabel == null || advancementLabel == null) throw new InvalidAdvancementException();
-		AdvancementTree tree = getAdvancementTree(treeLabel);
-		if (tree == null) throw new InvalidAdvancementException();
-		return tree.getAdvancement(advancementLabel);
-	}
+    /**
+     * Gets the correct advancement from the correct tree registered.
+     *
+     * @param path The path of an advancement formatted as 'treeLabel.advancementLabel'
+     * @return the {@link CAdvancement} belonging to that path
+     */
+    public CAdvancement getAdvancement(final String path) throws InvalidAdvancementException {
+        val treeLabel = getAdvancementTreeLabel(path);
+        val advancementLabel = getAdvancementLabel(path);
+        if (treeLabel == null || advancementLabel == null) throw new InvalidAdvancementException();
+        AdvancementTree tree = getAdvancementTree(treeLabel);
+        if (tree == null) throw new InvalidAdvancementException();
+        return tree.getAdvancement(advancementLabel);
+    }
 
-	/**
-	 * Gets the tree label from the given path
-	 * @param path The path of an advancement formatted as 'treeLabel.advancementLabel'
-	 * @return the tree label of the given path
-	 */
-	public static String getAdvancementTreeLabel(final String path) {
-		return path.split("\\.")[0];
-	}
+    /**
+     * Gets the tree label from the given path
+     *
+     * @param path The path of an advancement formatted as 'treeLabel.advancementLabel'
+     * @return the tree label of the given path
+     */
+    public static String getAdvancementTreeLabel(final String path) {
+        return path.split("\\.")[0];
+    }
 
-	/**
-	 * Gets the advancement label from the given path
-	 *
-	 * @param path The path of an advancement formatted as 'treeLabel.advancementLabel'
-	 * @return the advancement label of the given path
-	 */
-	public static String getAdvancementLabel(final String path) {
-		return path.split("\\.")[1];
-	}
+    /**
+     * Gets the advancement label from the given path
+     *
+     * @param path The path of an advancement formatted as 'treeLabel.advancementLabel'
+     * @return the advancement label of the given path
+     */
+    public static String getAdvancementLabel(final String path) {
+        return path.split("\\.")[1];
+    }
 
-	/**
-	 * Searches for the advancement type using the label it is registered with.
-	 *
-	 * @param type the label of an advancement type
-	 * @return the {@link AdvancementType}
-	 */
-	public AdvancementRewardType getAdvancementRewardType(final String type) {
-		return advancementRewardTypes.values().stream().filter(advancement -> advancement.equals(type)).findAny().orElseGet(None::new);
-	}
+    /**
+     * Searches for the advancement type using the label it is registered with.
+     *
+     * @param type the label of an advancement type
+     * @return the {@link AdvancementType}
+     */
+    public AdvancementRewardType getAdvancementRewardType(final String type) {
+        return advancementRewardTypes.values().stream().filter(advancement -> advancement.equals(type)).findAny().orElseGet(None::new);
+    }
 
-	/**
-	 * Gets the type of an requirement using the label
-	 *
-	 * @param type label of an registered {@link AdvancementRequirementType}
-	 * @return the {@link AdvancementRequirementType} that belongs to the given label
-	 */
-	public AdvancementRequirementType getAdvancementRequirementType(final String type) {
-		return advancementRequirementTypes.values().stream().filter(advancement -> advancement.equals(type)).findAny().orElseGet(me.tippie.customadvancements.advancement.requirement.types.None::new);
-	}
+    /**
+     * Gets the type of an requirement using the label
+     *
+     * @param type label of an registered {@link AdvancementRequirementType}
+     * @return the {@link AdvancementRequirementType} that belongs to the given label
+     */
+    public AdvancementRequirementType getAdvancementRequirementType(final String type) {
+        return advancementRequirementTypes.values().stream().filter(advancement -> advancement.equals(type)).findAny().orElseGet(me.tippie.customadvancements.advancement.requirement.types.None::new);
+    }
 
-	/**
-	 * Unregisters all listeners, {@link AdvancementType}'s, {@link AdvancementTree}'s, {@link AdvancementRewardType}'s, {@link AdvancementRequirementType}'s
-	 */
-	public void unregisterAll() {
-		advancementTypes.values().forEach(HandlerList::unregisterAll);
+    /**
+     * Unregisters all listeners, {@link AdvancementType}'s, {@link AdvancementTree}'s, {@link AdvancementRewardType}'s, {@link AdvancementRequirementType}'s
+     */
+    public void unregisterAll() {
+        advancementTypes.values().forEach(HandlerList::unregisterAll);
 
-		advancementTypes.clear();
-		advancementTrees.clear();
-		advancementRewardTypes.clear();
-		advancementRequirementTypes.clear();
-	}
+        advancementTypes.clear();
+        advancementTrees.clear();
+        advancementRewardTypes.clear();
+        advancementRequirementTypes.clear();
+    }
 }
--- core/src/main/java/me/tippie/customadvancements/util/Utils.java
+++ core/src/main/java/me/tippie/customadvancements/util/Utils.java
@@ -7,47 +7,12 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
 
+import java.util.regex.Pattern;
+
 public class Utils {
+    private final static Pattern NAMESPACED_KEY_PATTERN = Pattern.compile("^[a-z0-9/._-]+$");
 
-    // Format for JSON component:
-    // https://minecraft.fandom.com/wiki/Advancement/JSON_format
-    public static void showToast(Player player, String title, String description){
-        JsonObject advancementJSON = new JsonObject();
-
-        JsonObject display = new JsonObject();
-        display.addProperty("show_toast", true);
-        display.addProperty("announce_to_chat", false);
-        display.addProperty("hidden",true);
-        display.addProperty("frame", "task");
-        display.addProperty("title", title);
-        display.addProperty("description", description);
-
-        JsonObject icon = new JsonObject();
-        icon.addProperty("item", "minecraft:stone");
-        display.add("icon", icon);
-
-        //Creating an impossible critirion to grant it using the plugin
-        JsonObject criteria = new JsonObject();
-        JsonObject crit = new JsonObject();
-
-        crit.addProperty("trigger","minecraft:bee_nest_destroyed");
-        JsonObject conditions = new JsonObject();
-        conditions.addProperty("block", "minecraft:bedrock");
-        JsonObject item = new JsonObject();
-        item.addProperty("item", "minecraft:air");
-        conditions.add("item", item);
-        conditions.addProperty("num_bees_inside", 100);
-        criteria.add("crit",crit);
-        advancementJSON.add("criteria", criteria);
-
-
-        advancementJSON.add("display",display);
-        NamespacedKey key = new NamespacedKey(CustomAdvancements.getInstance(), "showtoast"+Math.round(Math.random()*10000));
-        Advancement advancement = Bukkit.getUnsafe().loadAdvancement(key, String.valueOf(advancementJSON));
-        for (String critirion : player.getAdvancementProgress(advancement).getRemainingCriteria()){
-            player.getAdvancementProgress(advancement).awardCriteria(critirion);
-        }
-
-        Bukkit.getUnsafe().removeAdvancement(key);
+    public static boolean validateNamespacedKey(String str) {
+        return NAMESPACED_KEY_PATTERN.matcher(str).matches();
     }
 }
